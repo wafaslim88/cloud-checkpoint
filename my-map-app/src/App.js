47288ccf80cb471fcb74331f3f26b9e694ad36b9
wafaslim88 <slimwafa88@gmail.com>
2023-07-18 11:25:52 +0100

    second commit

--- my-map-app/src/App.js
+++ my-map-app/src/App.js
@@ -1,47 +1,3 @@
-/*import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import './App.css';
-
-const AnyReactComponent = () => <div className="marker"></div>;
-
-const App = () => {
-  const location = {
-    address: 'ENIM Monastir, Monastir, Tunisia',
-    lat: 35.7520,
-    lng: 10.8005
-  };
-
-  const mapOptions = {
-    center: {
-      lat: location.lat,
-      lng: location.lng
-    },
-    zoom: 14
-  };
-
-  return (
-    <div className="app">
-      <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCdMDQd2GtOV_Au8lxpIbQLeMlwT9_ai8w' }}
-          defaultCenter={mapOptions.center}
-          defaultZoom={mapOptions.zoom}
-        >
-          <AnyReactComponent
-            lat={mapOptions.center.lat}
-            lng={mapOptions.center.lng}
-          />
-        </GoogleMapReact>
-      </div>
-      <div className="content">
-        <h1>Welcome to My Amazing App!</h1>
-        <p>Discover the world with Google Maps.</p>
-      </div>
-    </div>
-  );
-};
-
-export default App;*/
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import './App.css';
@@ -49,12 +5,13 @@ import './App.css';
 const AnyReactComponent = () => <div className="marker"></div>;
 
 const App = () => {
+   // Define the location (Eiffel Tower, Paris, France)
   const location = {
     address: 'Eiffel Tower, Paris, France',
     lat: 48.8584,
     lng: 2.2945
   };
-
+  // Define the map options
   const mapOptions = {
     center: {
       lat: location.lat,
@@ -66,14 +23,14 @@ const App = () => {
   return (
     <div className="app">
       <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{  key: 'AIzaSyCdMDQd2GtOV_Au8lxpIbQLeMlwT9_ai8w'  }}
-          defaultCenter={mapOptions.center}
-          defaultZoom={mapOptions.zoom}
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCdMDQd2GtOV_Au8lxpIbQLeMlwT9_ai8w' }} 
+          defaultCenter={mapOptions.center} // Set the default center of the map
+          defaultZoom={mapOptions.zoom} // Set the default zoom level of the map
         >
           <AnyReactComponent
-            lat={mapOptions.center.lat}
-            lng={mapOptions.center.lng}
+            lat={mapOptions.center.lat} // Pass the latitude of the center
+            lng={mapOptions.center.lng} // Pass the longitude of the center
           />
         </GoogleMapReact>
       </div>
